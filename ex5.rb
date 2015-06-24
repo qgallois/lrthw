99b3a14110b3b4614e33a17d99c7bc67df89c660
qgallois <q.gallois90@laposte.net>
2015-06-24 16:07:01 +0200

    Fini les exercices 1 à 6

---
+++ ex5.rb
@@ -0,0 +1,17 @@
+n = 'Quentin Gallois'
+a = 25 # Born in 1990
+he = 175 # cm
+w = 54 # kg
+e = 'Brown'
+t = 'White'
+ha = 'Brown'
+
+puts "Let's talk about #{n}."
+puts "He's #{he} inches tall."
+puts "He's #{w} pounds heavy."
+puts "Actually that's not too heavy."
+puts "He's got #{e} eyes and #{ha} hair."
+puts "His teeth are usually #{t} depending on the coffee."
+
+# this line is tricky, try to get it exactly right
+puts "If I add #{a}, #{he}, and #{w} I get #{a + he + w}."
